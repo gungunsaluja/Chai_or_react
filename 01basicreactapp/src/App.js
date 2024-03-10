ceb6f22f531db6876753ebb580a38a6d65736e8e
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-10 16:01:37 +0530

    react

---
+++ 01basicreactapp/src/App.js
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  return (
+    <h1>CHai |Gungun Saluja</h1>
+     
+  );
+}
+
+export default App;
+
+// <!-- first command should benpm create vite@latest 
+// then give name 
+// select react
+// varient as javascript
+// npm install in cd basic react app 
+// run npm run dev
+// to start server
+
+// -->
